a663a0b2142e2ad3b97a008fe60ea99d6a70b022
Sakirullahi <opshaktvl@gmail.com>
2021-07-30 18:51:36 +0530

    added SearchBar component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
 
 const App = () => {
   useEffect(() => {
     //Initializes materialize js
     M.AutoInit();
   }, []);
-  return <div className='App'>IT Logger</div>;
+  return <SearchBar />;
 };
 
 export default App;
