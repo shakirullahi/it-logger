a9094b8f5e387564aa536c013e1b985c68c48fe3
Sakirullahi <opshaktvl@gmail.com>
2021-08-04 18:16:02 +0530

    logs action and reducer setup

---
+++ src/actions/logActions.js
@@ -0,0 +1,41 @@
+import {
+    GET_LOGS, SET_LOADING, LOGS_ERROR
+} from "./types";
+
+// export const getLlogs = ()=>{
+//     return async (dispatch) =>{
+//         setLoading();
+//         const res = await fetch('/logs');
+//         const data = await res.json();
+
+//         dispatch({
+//             type:GET_LOGS,
+//             payload:data
+//         });
+//     }
+// }
+
+export const getLlogs = async dispatch =>{
+    try {
+        setLoading();
+        const res = await fetch('/logs');
+        const data = await res.json();
+
+        dispatch({
+            type:GET_LOGS,
+            payload:data
+        });
+    } catch (error) {
+        dispatch({
+            type:LOGS_ERROR,
+            payload:error.response.data
+        });
+    }
+}
+
+//set loading to true
+export const setLoading = () =>{
+    return {
+        type: SET_LOADING
+    }
+}
