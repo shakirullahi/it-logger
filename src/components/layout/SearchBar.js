a30984a0f47cdea7e93432efeb2bc354bb5cd0d2
Sakirullahi <opshaktvl@gmail.com>
2021-08-05 12:55:30 +0530

    seach bar setup

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { searchLogs } from "../../actions/logActions";
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const keyword = useRef("");
+
+  const onClose = () => {
+    // setKeyword("");
+    searchLogs("");
+  };
+
+  const onChange = () => {
+    searchLogs(keyword.current.value);
+  };
+
   return (
     <nav style={{ maringBottom: "20px" }} className='blue'>
       <div className='nav-wrapper'>
         <form>
           <div className='input-field'>
-            <input id='search' type='search' />
+            <input
+              id='search'
+              type='search'
+              ref={keyword}
+              onChange={onChange}
+            />
             <label className='label-icon' htmlFor='search'>
               <i className='material-icons'>search</i>
             </label>
-            <i className='material-icons'>close</i>
+            <i className='material-icons' onClick={onClose}>
+              close
+            </i>
           </div>
         </form>
       </div>
@@ -18,4 +39,8 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, { searchLogs })(SearchBar);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from "../actions/types";
 
 const initialState = {
@@ -50,6 +51,12 @@ export default (state = initialState, action) => {
         loading: false,
       };
     case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case SEARCH_LOGS:
       return {
         ...state,
         logs: action.payload,
